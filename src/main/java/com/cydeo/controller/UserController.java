5c7622bd77c207e04a32098dd608271766a0a8e3
Agabeg8 <agabegh@gmail.com>
2022-10-24 11:05:07 +0200

    Role DTO added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/create")
-    public String createUser(){
+    public String createUser(Model model){
+
+
+
         return "user/create";
     }
 }
