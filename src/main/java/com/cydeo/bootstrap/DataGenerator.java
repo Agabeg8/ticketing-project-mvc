916001858840bf7abede2a81b9a04d82e6656085
Agabeg8 <agabegh@gmail.com>
2022-10-27 10:42:36 +0200

    Role Updated

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -33,22 +33,22 @@ public class DataGenerator implements CommandLineRunner {
         roleService.save(managerRole);
         roleService.save(employeeRole);
 
-        UserDTO user1 = new UserDTO("John", "Kesy",
-                "john@cydeo.com", "Absc1", true, "45656765767", managerRole, Gender.MALE);
+        UserDTO user1 = new UserDTO("John", "Ksy",
+                "john@cydeo.com", "Abc1", true, "45656765767", managerRole, Gender.MALE);
         UserDTO user2 = new UserDTO("Mike", "Smith",
-                "mike@cydeo.com", "Absc2", true, "54654656546", managerRole, Gender.MALE);
+                "mike@cydeo.com", "Abc2", true, "54654656546", adminRole, Gender.MALE);
         UserDTO user3 = new UserDTO("Day", "Can",
-                "john@cydeo.com", "Absc3", true, "45654654654", managerRole, Gender.MALE);
+                "day@cydeo.com", "Abc3", true, "45654654654", employeeRole, Gender.MALE);
         UserDTO user4 = new UserDTO("Alice", "Com",
-                "john@cydeo.com", "Absc4", true, "56464656655", managerRole, Gender.FEMALE);
+                "alice@cydeo.com", "Abc4", true, "56464656655", managerRole, Gender.FEMALE);
         UserDTO user5 = new UserDTO("Maria", "Defoe",
-                "john@cydeo.com", "Absc4", true, "67676767676", managerRole, Gender.MALE);
-        UserDTO user6 = new UserDTO("John", "Kesy",
-                "john@cydeo.com", "Absc5", true, "23242343432", managerRole, Gender.MALE);
-        UserDTO user7 = new UserDTO("John", "Kesy",
-                "john@cydeo.com", "Absc3", true, "12323332323", managerRole, Gender.MALE);
-        UserDTO user8 = new UserDTO("John", "Kesy",
-                "john@cydeo.com", "Absc4", true, "99898798999", managerRole, Gender.MALE);
+                "maria@cydeo.com", "Abc5", true, "67676767676", employeeRole, Gender.FEMALE);
+        UserDTO user6 = new UserDTO("hella", "Ksy",
+                "hella@cydeo.com", "Abc6", true, "23242343432", employeeRole, Gender.FEMALE);
+        UserDTO user7 = new UserDTO("Bred", "Fool",
+                "bred@cydeo.com", "Abc7", true, "12323332323", employeeRole, Gender.MALE);
+        UserDTO user8 = new UserDTO("Nai", "Kia",
+                "nai@cydeo.com", "Abc8", true, "99898798999", employeeRole, Gender.MALE);
 
         userService.save(user1);
         userService.save(user2);
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model){
 
@@ -17,7 +27,8 @@ public class UserController {
 
         model.addAttribute("user", new UserDTO());
 
-        //model.addAttribute("roles" ,LISTOFROLES);
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "user/create";
     }
