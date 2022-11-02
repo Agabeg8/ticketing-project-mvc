9836e5fba777622e95acf440a4d5031b898f93e4
Agabeg8 <agabegh@gmail.com>
2022-11-02 12:21:09 +0100

    Updated

--- src/main/java/com/cydeo/converter/UserDtoConverter.java
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -12,6 +12,10 @@ public class UserDtoConverter implements Converter<String, UserDTO> {
 
     UserService userService;
 
+    public UserDtoConverter(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public UserDTO convert(String source) {
         return userService.findById(source);
