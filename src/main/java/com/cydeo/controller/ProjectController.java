7737ea1f1983616d1b578a357055d78a6fb287ec
Agabeg8 <agabegh@gmail.com>
2022-11-02 14:40:51 +0100

    Complete updated

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,6 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -26,7 +27,7 @@ public class ProjectController {
 
        model.addAttribute("project",new ProjectDTO());
        model.addAttribute("projects",projectService.findAll());
-       model.addAttribute("managers",userService.findAll());
+       model.addAttribute("managers",userService.findManagers());
 
 
         return "/project/create";
@@ -38,5 +39,20 @@ public class ProjectController {
         projectService.save(project);
 
         return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectcode}")
+    public String deleteProject(@PathVariable("projectcode") String projectcode){
+
+        projectService.deleteById(projectcode);
+        return "redirect:/project/create";
+
+    }
+    @GetMapping("/complete/{projectcode}")
+    public String completeProject(@PathVariable("projectcode") String projectcode){
+
+        projectService.complete(projectService.findById(projectcode));
+        return "redirect:/project/create";
+
     }
 }
